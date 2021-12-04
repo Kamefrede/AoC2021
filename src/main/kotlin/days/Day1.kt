a2ae93d5ca6a477cc419bd8f5175bc7745369a7c
Kamefrede <kamefrede@gmail.com>
2021-12-04 15:12:51 +0000

    feat: add 2021 day one

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,27 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        val numArray = inputList.map { Integer.parseInt(it) }
+
+        return numArray
+            .foldIndexed(0) { index, acc, number ->
+               if (index == 0) acc else if (numArray[index - 1] < number) (acc + 1) else acc
+            }
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        val numArray = inputList.map { Integer.parseInt(it) }
+        var a = numArray[0]
+        var b = numArray[1]
+        var c = numArray[2]
+        val answer = numArray.drop(3).fold(0) { acc, d ->
+            var newAcc = acc
+            if(b + c + d > a + b + c) newAcc = acc + 1
+            a = b
+            b = c
+            c = d
+            newAcc
+        }
+        return answer
     }
 }
