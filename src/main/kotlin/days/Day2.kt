029be57ad409e73eb8a263a43cceb36a4df8e394
Kamefrede <kamefrede@gmail.com>
2021-12-04 15:13:06 +0000

    feat: add 2021 day two

---
+++ src/main/kotlin/days/Day2.kt
@@ -0,0 +1,43 @@
+package days
+
+class Day2 : Day(2) {
+    override fun partOne(): Any {
+        var depth = 0
+        var horizonal = 0
+        inputList
+            .map {
+                val split = it.split(" ")
+                Pair(split[0], Integer.parseInt(split[1]))
+            }
+            .forEach { direction ->
+                when(direction.first[0]) {
+                    'u' -> depth -= direction.second
+                    'd' -> depth += direction.second
+                    'f' -> horizonal += direction.second
+                }
+            }
+        return depth * horizonal
+    }
+
+    override fun partTwo(): Any {
+        var depth = 0
+        var horizonal = 0
+        var aim = 0
+        inputList
+            .map {
+                val split = it.split(" ")
+                Pair(split[0], Integer.parseInt(split[1]))
+            }
+            .forEach { direction ->
+                when(direction.first[0]) {
+                    'u' -> aim -= direction.second
+                    'd' -> aim += direction.second
+                    'f' -> {
+                        horizonal += direction.second
+                        depth += aim * direction.second
+                    }
+                }
+            }
+        return depth * horizonal
+    }
+}
